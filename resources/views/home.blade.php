1d4e2456228e3ca760c203dd0e3a0b19ec523736
Andriy Bashuk <andreyb.efection@gmail.com>
2018-02-18 17:19:24 +0200

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Dashboard</div>
+                <passport-clients></passport-clients>
+                <passport-authorized-clients></passport-authorized-clients>
+                <passport-personal-access-tokens></passport-personal-access-tokens>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
